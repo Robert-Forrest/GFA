6132bcec97c4c8dfbbcfefff5973a0759a78712a
Robert Forrest <robertforrest@live.com>
2022-11-08 09:13:26 +0000

    Formatting and config update

--- __main__.py
+++ __main__.py
@@ -6,10 +6,10 @@ import cerebral as cb
 import data
 import composition_scan
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('config', default="config.yaml", nargs='?', type=str)
+    parser.add_argument("config", default="config.yaml", nargs="?", type=str)
 
     args = parser.parse_args()
 
@@ -17,67 +17,101 @@ if __name__ == '__main__':
 
     cb.setup(config=args.config)
 
-    if conf.task in ['simple', 'kfolds', 'kfoldsEnsemble',
-                     'tune', 'feature_permutation', 'compositionScan']:
+    if conf.task in [
+        "simple",
+        "kfolds",
+        "kfoldsEnsemble",
+        "tune",
+        "feature_permutation",
+        "compositionScan",
+    ]:
 
-        if conf.task == 'simple':
+        if conf.task == "simple":
             train_percentage = conf.train.get("train_percentage", 1.0)
             max_epochs = conf.train.get("max_epochs", 100)
 
-            originalData = cb.io.load_data(
-                postprocess=data.ensure_default_values)
+            originalData = cb.io.load_data(postprocess=data.ensure_default_values)
 
             if train_percentage < 1.0:
 
                 train, test = cb.features.train_test_split(
-                    originalData, train_percentage)
+                    originalData, train_percentage
+                )
 
-                train_compositions = list(train.pop('composition'))
-                test_compositions = list(test.pop('composition'))
+                train_compositions = list(train.pop("composition"))
+                test_compositions = list(test.pop("composition"))
 
-                train_ds, test_ds, train_features, test_features, train_labels, test_labels, sampleWeight, sampleWeightTest = cb.features.create_datasets(
-                    originalData, train, test)
+                (
+                    train_ds,
+                    test_ds,
+                    train_features,
+                    test_features,
+                    train_labels,
+                    test_labels,
+                    sampleWeight,
+                    sampleWeightTest,
+                ) = cb.features.create_datasets(originalData, conf.targets, train, test)
 
                 model = cb.models.train_model(
-                    train_features, train_labels, sampleWeight,
-                    test_features=test_features, test_labels=test_labels,
-                    sampleWeight_test=sampleWeightTest, maxEpochs=max_epochs)
+                    train_features,
+                    train_labels,
+                    sampleWeight,
+                    test_features=test_features,
+                    test_labels=test_labels,
+                    sampleWeight_test=sampleWeightTest,
+                    maxEpochs=max_epochs,
+                )
 
                 train_predictions = cb.models.evaluate_model(
-                    model, train_ds, train_labels, test_ds=test_ds, test_labels=test_labels,
-                    train_compositions=train_compositions, test_compositions=test_compositions)
+                    model,
+                    train_ds,
+                    train_labels,
+                    test_ds=test_ds,
+                    test_labels=test_labels,
+                    train_compositions=train_compositions,
+                    test_compositions=test_compositions,
+                )
 
             else:
-                compositions = list(originalData.pop('composition'))
+                compositions = list(originalData.pop("composition"))
 
-                train_ds, train_features, train_labels, sampleWeight = cb.features.create_datasets(
-                    originalData)
+                (
+                    train_ds,
+                    train_features,
+                    train_labels,
+                    sampleWeight,
+                ) = cb.features.create_datasets(originalData)
 
                 model = cb.models.train_model(
-                    train_features, train_labels, sampleWeight, maxEpochs=max_epochs)
+                    train_features, train_labels, sampleWeight, maxEpochs=max_epochs
+                )
 
                 train_predictions = cb.models.evaluate_model(
-                    model, train_ds, train_labels, train_compositions=compositions)
+                    model, train_ds, train_labels, train_compositions=compositions
+                )
 
-        elif conf.task == 'compositionScan':
+        elif conf.task == "compositionScan":
             composition_scan.run(compositions=conf.compositions)
 
         else:
-            if conf.task != 'feature_permutation':
+            if conf.task != "feature_permutation":
 
-                originalData = cb.io.load_data(
-                    postprocess=data.ensure_default_values)
+                originalData = cb.io.load_data(postprocess=data.ensure_default_values)
 
-                if conf.task == 'kfolds':
+                if conf.task == "kfolds":
                     cb.kfolds.kfolds(originalData)
 
-                elif conf.task == 'kfoldsEnsemble':
+                elif conf.task == "kfoldsEnsemble":
                     cb.kfolds.kfoldsEnsemble(originalData)
 
-                elif conf.task == 'tune':
+                elif conf.task == "tune":
 
-                    train_ds, train_features, train_labels, sampleWeight = cb.features.create_datasets(
-                        originalData)
+                    (
+                        train_ds,
+                        train_features,
+                        train_labels,
+                        sampleWeight,
+                    ) = cb.features.create_datasets(originalData)
 
                     cb.tuning.tune(train_features, train_labels, sampleWeight)
             else:
--- composition_scan.py
+++ composition_scan.py
@@ -13,42 +13,47 @@ def run(compositions=None):
         exit()
     else:
         for i in range(len(compositions)):
-            compositions[i] = re.findall('[A-Z][^A-Z]*', compositions[i])
+            compositions[i] = re.findall("[A-Z][^A-Z]*", compositions[i])
 
     onlyPredictions = True
     plotExamples = True
 
-    model = cb.models.load(cb.conf.output_directory + '/model')
+    model = cb.models.load(cb.conf.output_directory + "/model")
 
     x_features = cb.conf.x_features
     y_features = cb.conf.y_features
 
     if plotExamples:
         originalData = cb.io.load_data(
-            model=model, plot=False,
+            model=model,
+            plot=False,
             dropCorrelatedFeatures=False,
             additionalFeatures=y_features,
-            postprocess=data.ensure_default_values)
+            postprocess=data.ensure_default_values,
+        )
     else:
         originalData = None
 
     for composition in compositions:
         if len(composition) == 2:
             eg.plots.plot_binary(
-                composition, model,
+                composition,
+                model,
                 originalData=originalData,
                 x_features=x_features,
-                y_features=y_features)
+                y_features=y_features,
+            )
 
         elif len(composition) == 3:
             eg.plots.plot_ternary(
-                composition, model,
-                originalData=originalData,
-                y_features=y_features)
+                composition, model, originalData=originalData, y_features=y_features
+            )
 
         elif len(composition) == 4:
             eg.plots.plot_quaternary(
-                composition, model,
+                composition,
+                model,
                 onlyPredictions=onlyPredictions,
                 originalData=originalData,
-                y_features=y_features)
+                y_features=y_features,
+            )
